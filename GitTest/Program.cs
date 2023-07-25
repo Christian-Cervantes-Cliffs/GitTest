d1ab9ea5842009453d9b03fdf759482df457cc86
A13864 <christian.cervantes@clevelandcliffs.com>
2023-07-25 10:41:14 -0500

    Add project files.

---
+++ GitTest/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+void DoSomething()
+{
+    var a = 3;
+    var b = 4;
+    Console.WriteLine(a + b);
+}
+Console.WriteLine("Hello, World!");
+
+var a = 1;
+
+Console.WriteLine(a + 1);
+DoSomething();
+
+
