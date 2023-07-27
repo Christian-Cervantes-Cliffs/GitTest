a43593fe581574fd17d6d461f66ba5cc6c5ff9a7
A13864 <christian.cervantes@clevelandcliffs.com>
2023-07-27 14:39:51 -0500

    adding some stuff

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GitTest.Class;
+
 void DoSomething()
 {
     var a = 3;
@@ -12,5 +14,19 @@ var a = 1;
 
 Console.WriteLine(a + 1);
 DoSomething();
+AnotherMethod();
+Class1 c1 = new Class1("1", 20, "777");
+c1.DoSomething();
+Class1 c2 = c1;
+c2.Age = 27;
+c2.Phone = "333";
+c2.DoSomething();
+
+var cList = new List<Class1>();
+cList.Add(c1);
+cList.Add(c2);
+c1.DoSomething(cList);
+
+ void AnotherMethod() => Console.WriteLine("Another method");
 
 
